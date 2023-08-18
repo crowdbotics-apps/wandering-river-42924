4eee60905cd6be01a3ca513d3a44dd39415a3257
Crowdbotics <team@crowdbotics.com>
2023-08-18 02:36:49 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "wandering-river-42924",
+  url: "https://wandering-river-42924.botics.co",
+  api: "https://wandering-river-42924.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
